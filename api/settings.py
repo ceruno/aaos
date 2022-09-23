1fd9693476f319a069f54f87ebaaa9afc7749285
Daniel <daniel@riedener.net>
2022-09-23 06:26:43 +0000

    updates

--- api/settings.py
+++ api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_celery_beat',
+    'django_celery_results',
     'corsheaders',
     'elasticapm.contrib.django',
     'config',
