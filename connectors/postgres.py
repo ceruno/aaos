e192fdd03535402c320a561c82a366513f3bb7ff
Daniel <daniel@riedener.net>
2023-01-24 05:59:23 +0000

    update

--- connectors/postgres.py
+++ connectors/postgres.py
@@ -1,15 +1,25 @@
 import json
 import psycopg2, psycopg2.extras
 
-conn = psycopg2.connect(
-    host="db",
-    database="sar-dev",
-    user="sar-dev",
-    password="")
+class PostgresAPI:
+    def __init__(self, config, password, item):
+        self.host = config["host"]
+        self.port = config["port"]
+        self.db = config["db"]
+        self.user = config["user"]
+        self.password = password
+        self.query = config["query"]
+        self.session = psycopg2.connect(
+            host=self.host,
+            database = self.db,
+            user = self.user,
+            password = self.password)
 
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-cur.execute("select * from public.\"Questionnaire\";")
-results = cur.fetchall()
-print(json.dumps(results))
-
-conn.rollback()
+    def get(self):
+        cur = self.session.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(self.query)
+        # cur.execute("select * from public.\"Questionnaire\";")
+        results = cur.fetchall()
+        #print(json.dumps(results))
+        return results
+        #conn.rollback()
