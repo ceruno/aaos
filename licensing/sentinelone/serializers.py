fe24c159aeb6545c4215234f6b12ba79176253af
Daniel <daniel@riedener.net>
2023-03-27 13:45:44 +0000

    fix unlimited accounts/sites

--- licensing/sentinelone/serializers.py
+++ licensing/sentinelone/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 class ExportSerializer(serializers.Serializer):
     item = serializers.CharField(max_length=200)
     target = serializers.CharField(max_length=200)
+    project = serializers.CharField(max_length=200)
 
     def create(self, validated_data):
         return Export(**validated_data)
