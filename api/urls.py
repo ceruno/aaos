22d5e4ab28fdbbb6668191c91c74df4b25b0e257
Daniel <daniel@riedener.net>
2023-03-29 13:34:27 +0000

    version update

--- api/urls.py
+++ api/urls.py
@@ -82,7 +82,7 @@ urlpatterns = [
         get_schema_view(
             title="AAOS",
             description="Analysis, Automation and Orchestration System",
-            version="0.1.5",
+            version="0.1.6",
         ),
         name="openapi-schema",
     ),
